6400accc42c9c3583362741484447e9f71f4d9ee
Ray725 <Ray725@users.noreply.github.com>
2017-11-23 00:41:34 -0500

    used Java set to only print unique input

--- data_structures/src/data_structures/ChapterOne.java
+++ data_structures/src/data_structures/ChapterOne.java
@@ -3,8 +3,8 @@ package data_structures;
 public class ChapterOne {
 	
 	public static void main(String[] args) {
-		String file = "/Users/raymondli/algorithmGrind/data_structures/src/data_structures/temp.txt";
-		ProblemOne.lastFourtyTwo(file);
+		String file = "/Users/raymondli/algorithmGrind/data_structures/src/data_structures/test.txt";
+		ProblemOne.printUnique(file);
 	}
 	
 }
--- data_structures/src/data_structures/ProblemOne.java
+++ data_structures/src/data_structures/ProblemOne.java
@@ -2,10 +2,31 @@ package data_structures;
 
 import java.io.*;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 import java.util.Stack;
 
 public class ProblemOne {
+	
+	static void printUnique(String fileName) {
+		Set<String> uniqueWord = new HashSet<String>();
+		try {
+			File file = new File(fileName);
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while((line = br.readLine()) != null) {
+				if(uniqueWord.add(line)) {
+					System.out.println(line);
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file: " + fileName);
+		} catch (IOException ex) {
+			System.out.println("Error reading file: " + fileName);
+		}
+	}
 	
 	static void lastFourtyTwo(String fileName) {
 		Deque<String> deque = new LinkedList<>();
